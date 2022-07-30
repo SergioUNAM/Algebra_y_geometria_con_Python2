a53aa522f49ac700b480519730fbc8a03711a7af
SergioUNAM <hhser@comunidad.unam.mx>
2022-07-30 02:53:57 -0500

    Standard operations finished

--- standard_operations.py
+++ standard_operations.py
@@ -1,4 +1,4 @@
-"""The following functions presented perform standard operations on matrices: addition, multiplication by a number and transposition.  """
+"""The following functions presented perform standard operations on matrices: addition, multiplication by a number and transposition."""
 
 """Las siguientes funciones presentadas realizan operaciones estándares con matrices: suma, multiplicación por un escalar y transposición"""
 
@@ -35,9 +35,48 @@ def matrix_substract(A, B):
         return C
 
 
-def run():
-    pass
+def matrix_transpose(A):
+    C = [[0 for j in range(len(A))] for i in range(len(A[0]))]
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            C[j][i] = A[i][j]
 
+    return C
+
+
+def print_matrix(X):
+    for row in X:
+        for element in row:
+            print(element, end=" ")
+        print()
+    print()
+
+
+def run():
+    A = [[1, 1], [2, 2]]
+    B = [[2, 2], [3, 3]]
+
+    print("Matrix A: ")
+    print_matrix(A)
+    print("Matrix B: ")
+    print_matrix(B)
+
+    print("Sum of matrices: ")
+    S = matrix_add(A, B)
+    print_matrix(S)
+
+    alpha = 3
+    print(f"Multiplication by a scalar alpha = {alpha}: ")
+    M = matrix_mult_by_scalar(A, 3)
+    print_matrix(M)
+
+    print("Rest of matrices: ")
+    R = matrix_substract(A, B)
+    print_matrix(R)
+
+    print("Matrix transpose: ")
+    T = matrix_transpose(A)
+    print_matrix(T)
 
 if __name__ == "__main__":
     run()
